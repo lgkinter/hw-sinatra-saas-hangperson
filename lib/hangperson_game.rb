c161123e74285c839fa345f08fa5b7065ae9b9b0
lgkinter <lgkinter@yahoo.com>
2016-06-30 11:22:46 -0400

    Complete part 1 to pass RSPEC tests

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -1,15 +1,41 @@
 class HangpersonGame
 
-  # add the necessary class methods, attributes, etc. here
-  # to make the tests in spec/hangperson_game_spec.rb pass.
-
+  attr_accessor :word, :word_with_guesses, :guesses, :wrong_guesses
   # Get a word from remote "random word" service
 
-  # def initialize()
-  # end
-  
   def initialize(word)
     @word = word
+    @word_with_guesses = Array.new(@word.length, '-').join
+    @guesses = ""
+    @wrong_guesses = ""
+  end
+
+  def guess(letter)
+    if letter !~ /[a-zA-Z]/
+      raise(ArgumentError)
+      return false
+    end
+    letter = letter.downcase
+    if !@guesses.include?(letter) && @word.include?(letter)
+      @word.split(//).each_with_index {|ltr, i| @word_with_guesses[i] = ltr if ltr == letter}
+      @guesses << letter
+      return true
+    elsif !@wrong_guesses.include?(letter) && !@word.include?(letter)
+      @wrong_guesses << letter
+      return true
+    elsif @wrong_guesses.include?(letter) || @guesses.include?(letter)
+      return false
+    end
+  end
+
+  def check_win_or_lose
+    if @word_with_guesses !~ /-/
+      return :win
+    elsif @wrong_guesses.length >= 7
+      return :lose
+    else
+      return :play
+    end
   end
 
   def self.get_random_word
